bd144f4d5232d74dc88f4bfcccf1b706f6c451df
Joe <josephnewton861@yahoo.co.uk>
2020-09-07 18:53:46 +0100

    Added components for anagram and palindrome inputs

---
+++ src/components/AnagramForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AnagramForm = () => {
+  return (
+    <div>
+      <input type="text" />
+      <input type="text" />
+      <button>Log anagram</button>
+    </div>
+  );
+};
+
+export default AnagramForm;
---
+++ src/components/PalindromeForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PalindromeForm = () => {
+  return (
+    <div>
+      <input type="text" />
+      <button>Log palindrome</button>
+    </div>
+  );
+};
+
+export default PalindromeForm;
